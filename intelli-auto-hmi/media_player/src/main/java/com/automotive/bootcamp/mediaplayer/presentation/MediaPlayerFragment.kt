6dc28a09420f58c0a2bfce56950b055dd3778438
DESKTOP-9CTV1D5\sobko <sobkod23@gmail.com>
2022-01-10 18:06:20 +0200

    [Update] feature - JKBC-2

--- intelli-auto-hmi/media_player/src/main/java/com/automotive/bootcamp/mediaplayer/presentation/MediaPlayerFragment.kt
+++ intelli-auto-hmi/media_player/src/main/java/com/automotive/bootcamp/mediaplayer/presentation/MediaPlayerFragment.kt
@@ -1,28 +1,16 @@
 package com.automotive.bootcamp.mediaplayer.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.automotive.bootcamp.mediaplayer.R
+import com.automotive.bootcamp.common.base.BaseFragment
 import com.automotive.bootcamp.mediaplayer.databinding.MediaPlayerFragmentBinding
-import com.automotive.bootcamp.mediaplayer.presentation.data.MediaAlbum
 import kotlinx.android.synthetic.main.media_player_fragment.*
 
-class MediaPlayerFragment : BaseFragment(Fra) {
-
+class MediaPlayerFragment : BaseFragment<MediaPlayerFragmentBinding>(MediaPlayerFragmentBinding::inflate) {
     private lateinit var viewModel: MediaPlayerViewModel
     private lateinit var mediaPlayerAdapter: MediaPlayerRecyclerViewAdapter
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.media_player_fragment, container, false)
-    }
 
     override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
         super.onViewCreated(itemView, savedInstanceState)
@@ -38,7 +26,7 @@ class MediaPlayerFragment : BaseFragment(Fra) {
     }
 
     private fun initRecyclerView(){
-        mediaPlayerAdapter = MediaPlayerRecyclerViewAdapter(requireContext())
+        mediaPlayerAdapter = MediaPlayerRecyclerViewAdapter()
         rvAlbums.apply {
             layoutManager = GridLayoutManager(requireContext(),3)
             adapter = mediaPlayerAdapter
--- intelli-auto-hmi/media_player/src/main/java/com/automotive/bootcamp/mediaplayer/presentation/MediaPlayerRecyclerViewAdapter.kt
+++ intelli-auto-hmi/media_player/src/main/java/com/automotive/bootcamp/mediaplayer/presentation/MediaPlayerRecyclerViewAdapter.kt
@@ -8,36 +8,34 @@ import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.automotive.bootcamp.mediaplayer.R
+import com.automotive.bootcamp.mediaplayer.databinding.MediaPlayerAlbumBinding
 import com.automotive.bootcamp.mediaplayer.presentation.data.MediaAlbum
 import kotlinx.android.synthetic.main.media_player_album.view.*
 
-class MediaPlayerRecyclerViewAdapter ( private val context: Context) :
+class MediaPlayerRecyclerViewAdapter :
     ListAdapter<MediaAlbum, MediaPlayerRecyclerViewAdapter.AlbumViewHolder>(AsyncDifferConfig.Builder(AlbumDiffCallBack()).build()) {
-    private var list: List<MediaAlbum> = ArrayList()
-
     override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-        //  holder.albumArt. = load an image to the Image View
-        holder.songTitle.text = list[position].songTitle
-        holder.singerName.text = list[position].singerName
-        holder.rootView.setOnClickListener {
-            // add smth
-        }
+        holder.bind(getItem(position))
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
-        return AlbumViewHolder(
-            LayoutInflater.from(context).inflate(
-                R.layout.media_player_album,
-                parent,
-                false
-            )
-        )
+        val view: View =  LayoutInflater.from(parent.context).inflate(
+            R.layout.media_player_album,
+            parent,
+            false)
+        return AlbumViewHolder(MediaPlayerAlbumBinding.bind(view))
     }
 
-    class AlbumViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val rootView = v.clRootView
-        val albumArt = v.ivAlbumArt
-        val songTitle = v.tvSongTitle
-        val singerName = v.tvSingerName
+    class AlbumViewHolder(val binding: MediaPlayerAlbumBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(album: MediaAlbum){
+         // v.ivAlbumArt. = album.artUrl;
+            binding.apply {
+                tvSingerName.text = album.singerName
+                tvSongTitle.text = album.songTitle
+                root.setOnClickListener {
+                    // add smth
+                }
+            }
+        }
     }
 }
